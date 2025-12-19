5241c0480b8d8c2c879e3e7b63bf811602acce97
Adam Spofford <93943719+adamspofford-dfinity@users.noreply.github.com>
2025-12-18 20:21:07 -0800

    revise (#6)

--- Formula/icp-cli-network-launcher.rb
+++ Formula/icp-cli-network-launcher.rb
@@ -4,13 +4,13 @@ class IcpCliNetworkLauncher < Formula
   url "https://github.com/dfinity/icp-cli-network-launcher/archive/refs/tags/v11.0.0.tar.gz"
   sha256 "d4cf98ef6cd721533318124bc600a344e17391f764d3663f755b2539bb6035ba"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
-    root_url "https://github.com/adamspofford-dfinity/homebrew-icp/releases/download/icp-cli-network-launcher-11.0.0_1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8cc0f472500624cb464b7fe7747649875eaea0e48edc16cdaacd8bee0b4bd65a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c12dee4e0b6e0a1f9b8c41b44a8569c1e32d00a7724e55a00b42dba9f8db1bef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c1725655efdf999c5a744ea51d9024557af40b2b54e7df7786d21ebb3a5e9f2b"
+    root_url "https://github.com/adamspofford-dfinity/homebrew-icp/releases/download/icp-cli-network-launcher-11.0.0_2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "534fcc049066b1723d2fb3ca4f74693425b9f2b4d3cbfa33dcf0438f5f217770"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8ca4316fd65c4d4f1d9a1061fe163b8ea9b6db3b2bf762295f1ebf9ebb6f5671"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e056eb055540fc469f78c90f0bc136b3204610f6a3236684056d773163fdb42e"
   end
 
   depends_on "jq" => :build
